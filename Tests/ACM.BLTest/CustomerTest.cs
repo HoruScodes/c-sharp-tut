51ecd6b5f9c8a70d2f92524c0cf3f8534e1bf3de
raahulll <pipaliya1997@gmail.com>
2020-05-26 14:27:51 -0400

    Add project files.

---
+++ Tests/ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ACM.BL;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+
+        public void FullNameTestValdation()
+        {
+
+            //-- Arrange
+            Customer customer = new Customer();
+
+            customer.FirstName = "Rahul";
+            customer.LastName = "Pipaliya";
+
+            string expected = "Pipaliya,Rahul";
+
+
+            //-- Act
+            string actual = customer.FullName;
+
+            //--Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void FirstNameEmptyTestValdation()
+        {
+
+            //-- Arrange
+            Customer customer = new Customer();
+
+            customer.LastName = "Pipaliya";
+
+            string expected = "Pipaliya";
+
+
+            //-- Act
+            string actual = customer.FullName;
+
+            //--Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
+
+    
+
+
